9fbdb0b6292b2b3995da85762968146f36462202
Patrick Conroy <patrick@conroy-family.net>
2016-06-17 12:28:59 -0600

    Added some useful Shell Scripts to repo

---
+++ checkHHB.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+FNAME=/tmp/checkHHB.FAILURE.txt
+
+pgrep homeheart
+if [ $? -ne 0 ] ; then
+   date >> $FNAME
+   echo " --- HomeheartBeat has stopped, restarting " >> $FNAME
+   cd /home/pconroy/HomeHeartBeat 
+   ./dist/Debug/GNU-Linux-x86/homeheartbeat_1 &
+fi
+
